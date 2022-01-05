625b7031a14861b40758dc7e3f198e0c96bed90f
aigul <aigul.erma@gmail.com>
2022-01-05 16:31:36 -0400

    Holiday VIII - Duty Free

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function shortcut(string){
-    const vowels = /[aeiou]/g
-    return string.replace(vowels, '')
+function dutyFree(normPrice, discount, hol){
+let bott;
+return bott=Math.floor(hol / ((normPrice * discount) / 100));
+
 }
+
+
+console.log(dutyFree(17, 10 ,500));
