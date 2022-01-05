a06ca6c49261589fd4adc2d65a0528f4c74f2b7a
aigul <aigul.erma@gmail.com>
2022-01-04 22:40:16 -0400

    Well of Ideas - Easy Version

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function paperwork(n, m) {
- if (n > 0 && m > 0) {
-     return n * m
- } else {
-     return 0;
- }
+function well(x) {
+    let count = 0;
+    for (let i = 0; i <= x.length; i++) {
+        if (x[i] === 'good') {
+            count ++
+        }
+    }
+    if(count <= 2 && count > 0) {
+        return "Publish!"
+    }
+    if(count > 2) {
+        return "I smell a series!"
+    }
+    if(count <= 0) {
+        return "Fail!"
+    }
 }
