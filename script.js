770eec740b20dfe4ee75c57850233c74af35074a
aigul <aigul.erma@gmail.com>
2022-01-05 11:27:31 -0400

    Vowel remover

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function well(x) {
-    let count = 0;
-    for (let i = 0; i <= x.length; i++) {
-        if (x[i] === 'good') {
-            count ++
-        }
-    }
-    if(count <= 2 && count > 0) {
-        return "Publish!"
-    }
-    if(count > 2) {
-        return "I smell a series!"
-    }
-    if(count <= 0) {
-        return "Fail!"
-    }
+function shortcut(string){
+    const vowels = /[aeiou]/g
+    return string.replace(vowels, '')
 }
