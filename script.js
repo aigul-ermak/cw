1641d071d0a5e312f959142656147cdcf7a52fbe
aigul <aigul.erma@gmail.com>
2022-01-06 17:04:44 -0400

    Thinkful - Logic Drills: Traffic light

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const rps = (p1, p2) => {
-    let getMsg = ''
-    if( p1 == 'scissors' && p2 == 'paper' || p1 == 'rock' && p2 == 'scissors' ||  p1 == 'paper' && p2 == 'rock' ) {
-        return getMsg = 'Player 1 won!'
+function updateLight(current) {
+    let newLight = ''
+
+    if (current === "red") {
+        return newLight = "green"
     }
-    if (p1 == 'scissors' && p2 == 'rock' || p1 == 'paper' && p2 == 'scissors' || p1 == 'rock' && p2 == 'paper') {
-        return getMsg = 'Player 2 won!'
+    if ( current === "green") {
+        return newLight = "yellow"
     }
-    if (p1 == 'paper' && p2 == 'paper' || p1 == 'scissors' && p2 == 'scissors' || p1 == 'rock' && p2 == 'rock') {
-        return getMsg = 'Draw!'
+    if (current === "yellow") {
+        return newLight = "red"
     }
-};
+
+}
