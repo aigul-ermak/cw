1f80fef38110fd026b585733ba1961291410b5d4
aigul <aigul.erma@gmail.com>
2022-01-06 11:07:45 -0400

    Rock Paper Scissors!

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function smash (words) {
-    return words.join(' ').trim();
-
+const rps = (p1, p2) => {
+    let getMsg = ''
+    if( p1 == 'scissors' && p2 == 'paper' || p1 == 'rock' && p2 == 'scissors' ||  p1 == 'paper' && p2 == 'rock' ) {
+        return getMsg = 'Player 1 won!'
+    }
+    if (p1 == 'scissors' && p2 == 'rock' || p1 == 'paper' && p2 == 'scissors' || p1 == 'rock' && p2 == 'paper') {
+        return getMsg = 'Player 2 won!'
+    }
+    if (p1 == 'paper' && p2 == 'paper' || p1 == 'scissors' && p2 == 'scissors' || p1 == 'rock' && p2 == 'rock') {
+        return getMsg = 'Draw!'
+    }
 };
-
-console.log(smash (['hello', 'Hi', 'Goodbuy']))
