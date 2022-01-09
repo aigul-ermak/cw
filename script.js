5ed0b41104d3f02ad7a30223dcb7757b0a6282a0
aigul <aigul.erma@gmail.com>
2022-01-09 11:03:36 -0400

    Drink about

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+function peopleWithAgeDrink(old) {
+    let answer = "";
+
+    if (old < 14) {
+        return answer = "drink toddy"
+    } else if (old >= 14 && old <= 17) {
+        return answer = "drink coke"
+    } else if (old > 17 && old < 21) {
+        return answer = "drink beer"
+    } else {
+        return answer = "drink whisky"
+    }
 
 
-var moveZeros = function (arr) {
+};
 
-    let newArr = [];
-    let oldArr =[];
-    let totalArr = [];
-
-    arr.forEach(item => {
-
-        if (item === 0) {
-            newArr.push(item)
-        } else {
-            oldArr.push(item)
-        }
-    })
-    return totalArr = oldArr.concat(newArr);
-}
-
-
-console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]));
+console.log(peopleWithAgeDrink(30))
