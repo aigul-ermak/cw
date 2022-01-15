7b769e5d2c45a3af634987b94e7b8a664731269c
aigul <aigul.erma@gmail.com>
2022-01-15 10:17:13 -0400

    Square Every Digit

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function solution(str, ending){
-    return str.endsWith(ending)
+function squareDigits(num) {
 
+    let newArr = num.toString().split('')
+    let arr = []
+
+    for (let i = 0; i < newArr.length; i++) {
+        let n = newArr[i] * newArr[i]
+        arr.push(n)
+    }
+    let s = Number(arr.join(''))
+    return s
 }
 
-// console.log(solution('abcde', 'cde'))
-console.log(solution('sumo', 'omo'))
+console.log(squareDigits('2112'))
+
+console.log(typeof (squareDigits('2112')))
