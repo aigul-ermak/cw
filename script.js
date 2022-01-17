ea63dcccaa25587d330037e7b066fef867f45e82
aigul <aigul.erma@gmail.com>
2022-01-17 17:24:01 -0400

    Count of positives / sum of negatives

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function even_or_odd(number) {
-let a = number % 2
-    if( a === 0) {
-        return "Even"
-    } else {
-        return "Odd"
+function countPositivesSumNegatives(input) {
+    if (input === null || input.length < 1 ) {
+        return []
     }
+    let arr = []
+    let sumPlus = 0
+    let sumMinus = 0
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+            sumPlus += 1
+        } else {
+            sumMinus += input[i]
+        }
+    }
+    arr.push(sumPlus, sumMinus)
+
+    return arr
 }
 
-console.log(even_or_odd(2))
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
