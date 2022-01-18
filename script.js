7c806557542d5a9b34f367f0122b66bf307938f1
aigul <aigul.erma@gmail.com>
2022-01-18 16:43:30 -0400

    Returning Strings

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-function countPositivesSumNegatives(input) {
-    if (input === null || input.length < 1 ) {
-        return []
-    }
-    let arr = []
-    let sumPlus = 0
-    let sumMinus = 0
-
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] > 0) {
-            sumPlus += 1
-        } else {
-            sumMinus += input[i]
-        }
-    }
-    arr.push(sumPlus, sumMinus)
-
-    return arr
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
 }
 
-console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
+console.log(greet('Ryan'))
