22e19da3ccbcba54c2df7768848acabea785b7ae
aigul <aigul.erma@gmail.com>
2022-01-18 17:42:07 -0400

    Function 1 - hello world

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function getMiddle(s)
-{
-    let arr = s.split('')
-    let mid = Math.floor(arr.length / 2)
-    return  arr.length % 2 !== 0 ? arr[mid] : arr[mid -1 ] + arr[mid]
+function  greet() {
+    return "hello world!"
 }
+
