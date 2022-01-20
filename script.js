016502dd9b0f47da35fa1dd959d31d510530926b
aigul <aigul.erma@gmail.com>
2022-01-19 22:34:48 -0400

    Beginner Series #2 Clock

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function  greet() {
-    return "hello world!"
+function past(h, m, s){
+   let mS = (h * 60 * 60 * 1000) + (m * 60 *1000) + (s *1000)
+    return mS
 }
 
+console.log(past(1, 0, 1))
