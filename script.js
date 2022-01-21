9b2d2b0e59d6ddfa7e01ef6206b3fe50638610d3
aigul <aigul.erma@gmail.com>
2022-01-21 16:49:50 -0400

    Fake Binary

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function check(a, x) {
-    let b = a.includes(x)
-    return b === true;
+function fakeBin(x) {
+    let res = x.replace(/1|4|2|3/gi, '0')
+    let res1 = res.replace(/5|6|7|8|9/gi, '1')
+    return res1;
 }
 
-console.log(check(['t', 'e', 's', 't'], 'e'))
+console.log(fakeBin('45385593107843568'))
