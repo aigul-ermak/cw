aaf1beea467589a784b690c4f5de21864afdb292
aigul <aigul.erma@gmail.com>
2022-01-25 22:41:27 -0400

    Exes and Ohs

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function lovefunc(flower1, flower2){
-    let f = flower1 % 2
-    let f2 = flower2 % 2
-    return f !== f2 ? true : false
+function XO(str) {
+    let a = str.split('')
+    let countX = 0
+    let countO = 0
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === 'x' || a[i] === 'X') {
+            countX += 1
+        }
+    }
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === 'o' || a[i] === 'O') {
+            countO += 1
+        }
+    }
+
+    return countX === countO ;
 }
+
+console.log(XO("xo"))
+
+
