b1cdfe3574d806e596ee968cdaedbd9bdd75d958
aigul <aigul.erma@gmail.com>
2022-01-25 23:05:20 -0400

    Reverse words

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function XO(str) {
-    let a = str.split('')
-    let countX = 0
-    let countO = 0
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] === 'x' || a[i] === 'X') {
-            countX += 1
-        }
-    }
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] === 'o' || a[i] === 'O') {
-            countO += 1
-        }
-    }
+function reverseWords(str) {
+   let a = str.split(' ')
+    return a.map((item) => item.toString().split('').reverse().join('')).join(' ')
 
-    return countX === countO ;
+
 }
 
-console.log(XO("xo"))
+console.log(reverseWords("This is an example!"))
 
 
