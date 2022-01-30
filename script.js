a0723f935e8fe4c8c40722f2cd4fca113edc3518
aigul <aigul.erma@gmail.com>
2022-01-30 17:42:44 -0400

    Categorize New Member

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function solution(nums){
-    if (nums !== null) {
-        return  nums.sort((a, b) => a - b)
-    } else {
-        return []
-    }
-
+function openOrSenior(data){
+    let newArr =[]
+    let a = data.map((m, i) => {
+        if (m[0] >= 55 && m[1] > 7) {
+            newArr.push('Senior')
+        } else {
+            newArr.push('Open')
+        }
+    } )
+    return newArr;
 }
+
+console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
 
 
