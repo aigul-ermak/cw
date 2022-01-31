8324cc0334b40c21a7a0d7b8653aad9c6116f7e5
aigul <aigul.erma@gmail.com>
2022-01-31 18:00:11 -0400

    Total amount of points

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function otherAngle(a, b) {
-    return 180 - (a + b)
+function points(games) {
+    let count = 0
+    let a = games.map(g => {
+        if (g[0] > g[2]) {
+            count += 3
+        } else if (g[0] === g[2]) {
+            count += 1
+        } else if (g[0] < g[2]) {
+            count += 0
+        }
+    })
+    return count
 }
 
 
+
+console.log(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]))
