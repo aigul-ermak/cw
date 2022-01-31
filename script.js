87b134744c257c2f48fa7097db691cffcce84ad4
aigul <aigul.erma@gmail.com>
2022-01-31 18:22:46 -0400

    How good are you really?

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-function sum(numbers) {
+// function betterThanAverage(classPoints, yourPoints) {
+//     let total = 0
+//     for (let i = 0; i < classPoints.length; i++) {
+//         total += classPoints[i]
+//     }
+//     let aver = total / classPoints.length
+//     if (aver > yourPoints) {
+//         return "False"
+//     } else {
+//         return "True"
+//     }
+// }
 
-    if (numbers.length !== null) {
-        return numbers.reduce((accum, item) => {
-           accum += item
-            return accum
-        }, 0)
-    } else {
-        return 0
-    }
+
+function betterThanAverage(classPoints, yourPoints) {
+   return yourPoints > classPoints.reduce((a,b) => a + b, 0)/classPoints.length
 }
 
 
-console.log(sum([]))
+
+
+console.log(betterThanAverage([2, 3], 5))
