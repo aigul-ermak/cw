af40b173574412328e7f80afc56b51d481724361
aigul <aigul.erma@gmail.com>
2022-01-31 17:42:32 -0400

    Are You Playing Banjo?

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-function areYouPlayingBanjo(name) {
-    let n = name.split('')
-    if (n[0] === 'R' | n[0] === 'r') {
-        return `${name} plays banjo`
-    } else {
-        return `${name} does not play banjo`
-    }
-
+function otherAngle(a, b) {
+    return 180 - (a + b)
 }
-
 
 
