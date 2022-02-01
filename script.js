be5ca8e33a2e85cd52573d6716220095882e3390
aigul <aigul.erma@gmail.com>
2022-02-01 17:22:58 -0400

    Mumbling

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// function betterThanAverage(classPoints, yourPoints) {
-//     let total = 0
-//     for (let i = 0; i < classPoints.length; i++) {
-//         total += classPoints[i]
-//     }
-//     let aver = total / classPoints.length
-//     if (aver > yourPoints) {
-//         return "False"
-//     } else {
-//         return "True"
-//     }
-// }
+function accum(s) {
+   let a = s
+       .split('')
+       .map((l, i) => l.toUpperCase() + l.repeat(i).toLowerCase())
 
-
-function betterThanAverage(classPoints, yourPoints) {
-   return yourPoints > classPoints.reduce((a,b) => a + b, 0)/classPoints.length
+   return a.join('-')
 }
 
 
 
 
-console.log(betterThanAverage([2, 3], 5))
+
+
+console.log(accum("abcd"))
