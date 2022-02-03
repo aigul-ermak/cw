8861680968d4d6e2ae3eaae3fbcef55ee4e18714
aigul <aigul.erma@gmail.com>
2022-02-02 22:18:04 -0400

    Grasshopper - Check for factor

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function getSum(a, b) {
-    const min = a < b ? a : b
-    const max = a < b ? b : a
-
-    let count = 0
-    for (let i = min; i <= max; i++) {
-        count += i
+function checkForFactor (base, factor) {
+    if (Number.isInteger(base / factor)) {
+        return true
+    } else {
+        return false
     }
 
-    return count
-
-
 }
-
-console.log(getSum(0, -1))
