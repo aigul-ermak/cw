8f81a9c9451b53050c0d63d82e734601c4c76006
aigul <aigul.erma@gmail.com>
2022-02-21 22:13:20 -0400

    zeroFuel

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-function sumArray(array) {
-    debugger
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 
-   if( !array) {
-       return 0
-   } else if (array.length < 0) {
-       return 0
-   } else {
-       array = array.sort((a, b) => a - b)
-       var total = 0
-       for (var i = 1; i < array.length - 1; i++) {
-           total += array[i]
-       }
-       return total
-   }
+    return mpg * fuelLeft >= distanceToPump
 
-}
+};
+
+console.log(zeroFuel(100, 50, 2))
 
 
-console.log(sumArray([ -6, -20, -1, -10, -12]))
 
-
-// function palindrome (str) {
-//     var re = /[^A-Za-z0-9]/g
-//     str = str.toLowerCase().replace(re, '')
-//     var len = str.length
-//     for (let i = 0; i < len/2; i++) {
-//         if(str[i] !== str[len - 1 - i]) {
-//             return false
-//         }
-//     }
-//     return true
-//
-// }
-//
-// console.log(palindrome("A man, a pl, a canal. Panama"))
