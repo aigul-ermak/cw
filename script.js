3019c4803f9f1e2a2a9517b318ab1ab7e89dbcfa
aigul <aigul.erma@gmail.com>
2022-02-22 11:51:10 -0400

    Grasshopper - Grade book

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function sumDigits(number) {
-    let a = number.toString().replace('-', '').split('')
-    let b = a.reduce((acc, item) => {
 
-        acc = acc + (+item)
-        return acc;
-    }, 0)
-    return b
+    function getGrade(s1, s2, s3) {
+        let score = (s1 + s2 + s3) / 3
+        if (score >= 90 && score <= 100) {
+            return 'A'
+        } else if (score >= 80 && score < 90) {
+            return 'B'
+        } else if (score >= 70 && score < 80) {
+            return 'C'
+        } else if (score >= 60 && score < 70) {
+            return 'D'
+        } else {
+            return 'F'
+        }
 }
 
 
-console.log(sumDigits(-14))
+console.log(getGrade(20, 10, 30))
 
 
 
