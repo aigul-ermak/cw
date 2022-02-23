6ab792a1cc4e4a31334ade12a68106995609857a
aigul <aigul.erma@gmail.com>
2022-02-22 21:15:40 -0400

    Binary Addition

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function move(position, roll) {
-    return nextPos = position + (roll * 2)
+function addBinary(a,b) {
+  return (a+b).toString(2)
 }
 
+console.log(addBinary(2,5))
 
-console.log(move(2, 5))
+
+// 0 0
+//1 1
+//10 2
+//11 3
+//100 4
+// 101 5
+//110 6
+//111 7
+//1000 8
+//1001 9
+//1010 10
+
 
 
 
