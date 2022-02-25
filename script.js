b7c7331c7a9377bf460b3fe983b6a8d0b638540f
aigul <aigul.erma@gmail.com>
2022-02-24 22:56:54 -0400

    Printer Errors

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-String.prototype.isUpperCase = function() {
-    return String(this) === this.toUpperCase()
+function printerError(s) {
+    let re = /[n-z]/gi
+    let arr = s.match(re)
+    let c
+    if(!arr) {
+        c = 0
+    } else {
+        c = arr.length
+    }
+
+    let a = s.split('')
+    let b =  a.length
+
+    let d = []
+    d.push(c)
+    d.push(b)
+
+    return d.join('/')
 }
 
 
-console.log('hello I AM DONALD'.isUpperCase())
+console.log(printerError("aaabbbbhaijjj"))
 
 
 
