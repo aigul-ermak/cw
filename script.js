a0d5b2f6f358b42c6d063b5b2ad16550a57ab0e2
aigul <aigul.erma@gmail.com>
2022-02-24 23:28:19 -0400

    Is it even?

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function minMax(arr){
-    let a = arr.sort((a, b) => a - b)
-    let b = []
-    b.push(a.slice(0, 1))
-    b.push(a.slice(-1))
-
-
-    return b.reduce((prev, item) => prev.concat(item))
+function testEven(n) {
+    function testEven(n) {
+        if (n % 2 === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
 
-console.log(minMax([2334454,5]))
 
 
 
