23a5ceb38c11032d9e93a5dcce5e9346988c551f
aigul <aigul.erma@gmail.com>
2022-02-26 14:34:40 -0400

    Difference of Volumes of Cuboids

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function greet (name, owner) {
-    if ( name === owner) {
-        return 'Hello boss'
-    } else {
-        return 'Hello guest'
-    }
+function findDifference(a, b) {
+    let c = (a[0] * a[1] * a[2])
+    let v = (b[0] * b[1] * b[2])
+
+    return c > v ? c - v : v - c
 }
+
+
+console.log(findDifference([3, 2, 5], [1, 4, 4]))
