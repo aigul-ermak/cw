8125d1a21808663d91e182b10c567414e1c1047e
aigul <aigul.erma@gmail.com>
2022-02-26 15:07:11 -0400

    Anagram Detection

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function findDifference(a, b) {
-    let c = (a[0] * a[1] * a[2])
-    let v = (b[0] * b[1] * b[2])
-
-    return c > v ? c - v : v - c
-}
+var isAnagram = function(test, original) {
+    return test.toLowerCase().split('').sort().join("") === original.toLowerCase().split('').sort().join("")
+};
 
 
-console.log(findDifference([3, 2, 5], [1, 4, 4]))
+
+console.log(isAnagram("dumble", "bumble"))
