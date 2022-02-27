59dcdec73d7cb30dd1c603c169758c458917b13c
aigul <aigul.erma@gmail.com>
2022-02-27 09:05:23 -0400

    Jaden Casing Strings

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var isAnagram = function(test, original) {
-    return test.toLowerCase().split('').sort().join("") === original.toLowerCase().split('').sort().join("")
+String.prototype.toJadenCase = function () {
+    let a = String(this).split(' ')
+
+    return a.map(m => m[0].toUpperCase() + m.slice(1)).join(' ')
+
 };
 
 
+console.log("How can mirrors be real if our eyes aren't real".toJadenCase())
 
-console.log(isAnagram("dumble", "bumble"))
