d79829f1e1c01f251de6b0b0f04f969137904606
aigul <aigul.erma@gmail.com>
2022-03-03 17:58:22 -0400

    Remove exclamation marks

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function problem(x){
-    if (typeof x === 'string') {
-        return 'Error'
-    } else {
-        return x * 50 + 6
-    }
+function removeExclamationMarks(s) {
+    let re = /!/g
+
+    return s.replaceAll(re, '');
 }
+
+
+console.log(removeExclamationMarks("Hello World!!"))
