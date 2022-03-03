14067701c4834c41140c816fabbb0b5235424eaa
aigul <aigul.erma@gmail.com>
2022-03-03 18:53:01 -0400

    Count the Digit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function replace(s){
-  let re = /[aeiouAEIOU]/gi
-    return s.replace(re, '!')
+function nbDig(n, d) {
+    let arr = []
+    let count = 0
+    for (let i = 0; i <= n; i++) {
+        arr.push(i * i)
+    }
+    let a = arr.toString().split('')
+
+    for (let j = 0; j <= a.length; j++) {
+        debugger
+        if (a[j] == d)
+            count = count + 1
+    }
+    return count
 }
 
-console.log(replace("Hi!"))
+console.log(nbDig(5750, 0))
