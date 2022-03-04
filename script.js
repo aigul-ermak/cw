e4bf8f15a799b09e101f8952fc3185cfcbc12797
aigul <aigul.erma@gmail.com>
2022-03-04 13:59:59 -0400

    N-th Power

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const binaryArrayToNumber = arr => {
-    let a = arr.join('')
-    return parseInt(a, 2)
-};
+function index(array, n) {
+    if(array.length > n) {
+        return array[n]**n
+    } else {
+        return -1
+    }
 
 
-console.log(binaryArrayToNumber([1,1,1,1]))
+}
+
+console.log(index([3,5], 1))
