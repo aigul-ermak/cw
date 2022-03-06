256018292884b9b13df4cb4dff3062d0f1465089
aigul <aigul.erma@gmail.com>
2022-03-06 17:40:16 -0400

    Will there be enough space?

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-function maxMultiple(divisor, bound) {
-    let arr = []
-
-    for (let i = 0; i <= bound; i++) {
-        if (i % divisor === 0) {
-            arr.push(i)
-        }
+function enough(cap, on, wait) {
+    if (cap > (on + wait)) {
+        return 0
+    } else if  ( cap === (on + wait)) {
+        return 0
+    } else if( cap < (on + wait)) {
+        return (on + wait) - cap
     }
-    let c = arr.reduce((acc, item) => {
-        if (acc === null || item > acc) {
-            return item
-        }
-    }, null)
-
-    return  c
-
 
 }
 
-console.log(maxMultiple(37, 200))
-//n / divisor   6/2
-// n < = bound 6 < 7
-// n > 0
+console.log(enough(10, 5, 5))
 
-// a % divisor === 0
-// a < = bound
-// a > 0
-//
-// return a
+
+
