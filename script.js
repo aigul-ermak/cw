ac02e731dbcd1483a15f4396f49cac8c3cd502c1
aigul <aigul.erma@gmail.com>
2022-03-05 22:37:44 -0400

    Check the exam

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function index(array, n) {
-    if(array.length > n) {
-        return array[n]**n
-    } else {
-        return -1
+function checkExam(array1, array2) {
+  let count = 0
+    for (let i = 0; i < array1.length ; i++) {
+
+        if (array2[i] === '') {
+            count = count + 0
+        } else  if (array1[i] === array2[i]) {
+            count = count + 4
+        } else if (array1[i] !== array2[i]) {
+            count = count - 1
+        }
     }
 
 
+    return count < 0 ? 0 : count
 }
 
-console.log(index([3,5], 1))
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
+//+4   -1   +0 if score < 0 return 0
