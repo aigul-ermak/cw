fe79cc55c79be9e818e239092cf7a49ef5f4a144
aigul <aigul.erma@gmail.com>
2022-03-09 18:33:12 -0400

    Describe the shape

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function capitalize(s) {
-    let a = s.split('')
-    let arr = []
-    let arr1 = []
-    for (let i = 0; i < a.length; i++) {
-        if (i % 2 === 0) {
-            arr.push(a[i].toUpperCase())
-            arr1.push(a[i])
-        } else if (i % 2 !== 0) {
-            arr.push(a[i])
-            arr1.push(a[i].toUpperCase())
-        }
+function describeTheShape(angles) {
+    let sum = 180 * (angles - 2)
+    let oneAngle = Math.round(sum / angles)
+    if (angles <= 2) {
+        return 'this will be a line segment or a dot'
+    } else {
+        return `This shape has ${angles} sides and each angle measures ${oneAngle}`
     }
 
-    return [arr.join(''), arr1.join('')]
-};
 
-console.log(capitalize("abcdef"))
+}
+
+console.log(describeTheShape(3))
