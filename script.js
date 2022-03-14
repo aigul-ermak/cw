da7648947db08cf5ef15cd64db3a2d9f69803c7e
aigul <aigul.erma@gmail.com>
2022-03-13 21:11:59 -0400

    Factorial

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function repeater(string, n){
-   return string.repeat(n)
+function factorial(n){
+    let count = 1
+    for (let i = 1; i <= n ; i++) {
+        count = count * i
+    }
+    return count
 }
 
-console.log(repeater('a', 5))
-// 10
+
+console.log(factorial(4))
