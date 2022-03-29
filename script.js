421abd03117d2dbf04b6e76145fee3dd790c092b
aigul <aigul.erma@gmail.com>
2022-03-28 22:38:24 -0300

    Add property to every object in array

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const last = xs => {
-    return xs.length === 0 ? null : xs[xs.length - 1]
+var questions = [
+    {
+        question: "What's the currency of the USA?",
+        choices: ["US dollar", "Ruble", "Horses", "Gold"],
+        corAnswer: 0
+    },
+    {
+        question: "Where was the American Declaration of Independence signed?",
+        choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+        corAnswer: 0
+    }];
 
-}
+
+// function addAnswer(array, property, value) {
+//     return array.map(m => ({...m, [property]: value}));
+// }
+
+questions.forEach(function(q) {
+    q.usersAnswer = null
+})
 
 
-console.log(last([1,2,3]))
+
+console.log(questions)
+// console.log(questions[0].usersAnswer === null)
