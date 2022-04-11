e5562679b19e465f5f877a6afbe0a62bfc79c51e
aigul <aigul.erma@gmail.com>
2022-04-11 20:17:56 -0300

    Check Same Case

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-var questions = [
-    {
-        question: "What's the currency of the USA?",
-        choices: ["US dollar", "Ruble", "Horses", "Gold"],
-        corAnswer: 0
-    },
-    {
-        question: "Where was the American Declaration of Independence signed?",
-        choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-        corAnswer: 0
-    }];
+function sameCase(a, b) {
+    if (/[a-z]/i.test(a) && /[a-z]/i.test(b)) {
+        return (a === a.toUpperCase() && b === b.toUpperCase() || (a === a.toLowerCase() && b === b.toLowerCase()))
+        ? 1 : 0
+    } else
+        return -1
+}
 
 
-// function addAnswer(array, property, value) {
-//     return array.map(m => ({...m, [property]: value}));
-// }
-
-questions.forEach(function(q) {
-    q.usersAnswer = null
-})
-
-
-
-console.log(questions)
-// console.log(questions[0].usersAnswer === null)
+console.log(sameCase('c', 'b'))
