e11fad13a68e5c5ef0949968773104c983dec6b8
aigul <aigul.erma@gmail.com>
2022-05-13 00:34:47 -0300

    Basic Training: Add item to an Array

---
+++ 8_kyu/cd-8-kyu-cd-Basic_Training_Add_item_to_an_Array.js
@@ -0,0 +1,18 @@
+/*
+
+Link: https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+
+ */
+
+
+const addWord = () => {
+    const websites = [];
+
+    websites.push("codewars")
+
+    return websites
+}
+
+
+console.log(addWord())
+
