e28b82f0873ec965b8c8c7ce84f6e2ca6e450c78
aigul <aigul.erma@gmail.com>
2022-05-14 23:45:52 -0300

    Are there doubles?

---
+++ 7_kyu/cd-7-kyu-Are_there_doubles.js
@@ -0,0 +1,11 @@
+/*
+
+Link: https://www.codewars.com/kata/56a24b4d9f3671584d000039/train/javascript
+
+ */
+
+const doubleCheck = (str) => /(.)\1/i.test(str)
+
+console.log(doubleCheck("a b c d e f g h i h k"))
+
+
