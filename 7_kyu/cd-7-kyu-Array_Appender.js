8ef765c6c2673eda89f231ac4ff46f8fb84919d7
aigul <aigul.erma@gmail.com>
2022-05-16 21:39:38 -0300

    Array Appender

---
+++ 7_kyu/cd-7-kyu-Array_Appender.js
@@ -0,0 +1,14 @@
+/*
+
+Link: https://www.codewars.com/kata/53a8a476947277a3020001cc/train/javascript
+
+ */
+
+function appendArrays (arr1, arr2) {
+
+  return arr1.concat(arr2)
+}
+
+console.log(appendArrays([1,2], [2,4]))
+
+
