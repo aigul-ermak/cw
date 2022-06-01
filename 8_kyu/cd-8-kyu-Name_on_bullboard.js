e9a736940ec17c7398e5dde69f359491bfddb232
aigul <aigul.erma@gmail.com>
2022-05-31 22:48:36 -0300

    Name on billboard

---
+++ 8_kyu/cd-8-kyu-Name_on_bullboard.js
@@ -0,0 +1,16 @@
+/*
+
+Link: https://www.codewars.com/kata/570e8ec4127ad143660001fd/train/javascript
+
+ */
+
+function billboard(name, price = 30) {
+    let count = 0;
+    const nameLength = name.split('')
+    for (let i = 0; i < nameLength.length; i++) {
+        count += price
+    }
+    return count
+}
+
+console.log(billboard('Jeong-Ho Aristotelis'));
