d4c42166d838170d5b6a3b12d07bb15599991479
aigul <aigul.erma@gmail.com>
2022-05-31 22:46:39 -0300

    Name on billboard

--- 7_kyu/cd-7-kyu-Average_Array.js
+++ 7_kyu/cd-7-kyu-Average_Array.js
@@ -1,22 +1,19 @@
 /*
 
-Link: https://www.codewars.com/kata/596f6385e7cd727fff0000d6/train/javascript
+Link: https://www.codewars.com/kata/570e8ec4127ad143660001fd/train/javascript
 
  */
 
-function avgArray(array) {
-
-    const initialArray = Array.from({length: array[0].length}, () => 0)
-    const totalValues = array.reduce((result, array) => {
-        array.forEach((value, index) => (result[index] += value))
-        return result
-    }, initialArray)
-    const totalAver = totalValues.map(value => value / array.length)
-    return totalAver;
-
+function billboard(name, price = 30) {
+    let count = 0;
+    const nameLength = name.split('')
+    for (let i = 0; i < nameLength.length; i++) {
+        count += price
+    }
+    return count
 }
 
-console.log(avgArray([[1, 2, 3, 4], [5, 6, 7, 8]]));
+console.log(billboard('Jeong-Ho Aristotelis'));
 
 
 
