04dc5706667bc90d9e75863bcaec37e5a5824329
aigul <aigul.erma@gmail.com>
2022-06-08 23:31:01 -0300

    Grasshopper - Debug sayHello

---
+++ 8_kyu/cd-8-kyu-Grasshopper_Debug_sayHello.js
@@ -0,0 +1,13 @@
+/*
+
+Link: https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/javascript
+
+ */
+
+
+function sayHello(name) {
+    return `Hello, ${name}`
+}
+
+console.log(sayHello('Liutenant Uhura'))
+
