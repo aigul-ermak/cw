49003fdd4bf897f2b6bc983162efae6214a1e31d
aigul <aigul.erma@gmail.com>
2022-07-05 09:27:46 -0300

    No oddities here

---
+++ 7_kyu/cd-7-kyu-No-oddities-here.js
@@ -0,0 +1,15 @@
+/*
+
+Link: https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+
+ */
+
+
+function noOdds( values ){
+   return values.filter(f => f % 2 === 0)
+}
+
+
+console.log(noOdds([0,1,2,3]))
+
+
